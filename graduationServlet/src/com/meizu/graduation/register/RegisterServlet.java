f94bdacb1161060433515f17b7573b6cbd57cf70
lizhi <lizhi@meizu.com>
2015-02-18 22:11:46 +0800

    register function

---
+++ graduationServlet/src/com/meizu/graduation/register/RegisterServlet.java
@@ -0,0 +1,42 @@
+package com.meizu.graduation.register;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import oracle.jrockit.jfr.settings.JSONElement;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	   Map<String, String[]> map=request.getParameterMap();
+	   String[] string=map.get("json");
+	   System.out.print(string[0]);
+	}
+
+}
