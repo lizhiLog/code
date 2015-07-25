a746cffbd1052f7bea931ad0d6d543bee7051bad
李志 <lizhi@meizu.com>
2015-07-25 09:21:51 +0800

    [云应用]变更内容：outh2 变更类别：资源替换 适用机型：all 验证建议：无 关联变更项：无 check by liumeng

---
+++ MyGrduate/app/build/generated/source/buildConfig/debug/com/meizu/lizhi/mygraduation/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.meizu.lizhi.mygraduation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.meizu.lizhi.mygrduate";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
